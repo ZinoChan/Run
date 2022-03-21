fde13646159fe6b19aa257417ae4894c64ca6685
Zineb ELGUIL <devzinochan@gmail.com>
2022-03-21 15:32:57 +0000

    upgrade to firebase 9 + refactor

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,60 +1,71 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import 'firebase/storage';
-
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+import { toast } from 'react-hot-toast';
 
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_API_KEY}`,  
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
   databaseURL: `${process.env.REACT_APP_DATABASE_URL}`,
   projectId: `${process.env.REACT_APP_PROJECT_ID}`,
   storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
   messagingSenderId: `${process.env.REACT_APP_MSG_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_APP_ID}`
+  appId: `${process.env.REACT_APP_APP_ID}`,
 };
 
-firebase.initializeApp(firebaseConfig);
+class Firebase {
+  constructor() {
+    this.app = initializeApp(firebaseConfig);
+    this.auth = getAuth(this.app);
+    this.db = getFirestore(this.app);
+    this.storage = getStorage(this.app);
+    this.googleAuthProvider = new GoogleAuthProvider();
+    this.collectionRef = collection(this.db, 'products');
+  }
 
-export const auth = firebase.auth();
-const db = firebase.firestore();
-
-export const storage = firebase.storage();
-
-export const addProduct = (id, product) => db.collection('products').doc(id).set(product);
-
-
-export const generateKey = () => db.collection('products').doc().id;
-export const signOut = () => firebase.auth().signOut();
-export const signInWithGoogle = () => auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-
-export const getProducts = () => {
+  signOut = () => signOut(this.auth);
+  signInWithGoogle = () => signInWithPopup(this.auth, this.googleAuthProvider);
+  getProducts = () => {
     let didTimeout = false;
 
     return new Promise(async (resolve, reject) => {
-      
-        const timeout = setTimeout(() => {
-          didTimeout = true;
-          reject('Request timeout, please try again');
-        }, 15000); 
+      const timeout = setTimeout(() => {
+        didTimeout = true;
+        reject('Request timeout, please try again');
+      }, 15000);
 
-        try {
-         
-          
-          const totalQuery = await db.collection('products').get();
+      try {
+        const totalQuery = await getDocs(this.collectionRef);
 
-          clearTimeout(timeout);
-          if (!didTimeout) {
-            const products = [];
-            totalQuery.forEach(doc => products.push({...doc.data() }));
-           
-            resolve(products);
-          }
-        } catch (e) {
-          if (didTimeout) return;
-          console.log('Failed to fetch products: An error occured while trying to fetch products or there may be no product ', e);
-          reject(':( Failed to fetch products.');
+        clearTimeout(timeout);
+        if (!didTimeout) {
+          const products = [];
+
+          totalQuery.forEach((doc) => products.push(doc.data()));
+
+          resolve(products);
         }
-      
+      } catch (e) {
+        if (didTimeout) return;
+        toast.error(
+          'Failed to fetch products: An error occured while trying to fetch products or there may be no product '
+        );
+
+        reject(':( Failed to fetch products.');
+      }
     });
+  };
 }
+
+const firebaseInstance = new Firebase();
+
+export default firebaseInstance;
