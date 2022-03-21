a2788ddb432efcd9e4d4487f32411fa9a8e2ed12
Zineb ELGUIL <devzinochan@gmail.com>
2022-03-21 17:53:47 +0000

    upgrade to react-router v6 + prettier formatting

--- src/routes/AppRoute.js
+++ src/routes/AppRoute.js
@@ -1,42 +1,92 @@
-import React, {Suspense, lazy} from 'react'
-import { Switch, useLocation } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
-import PublicRoute from './PublicRoute';
-import PrivateRoute from './PrivateRoute';
-import 'react-toastify/dist/ReactToastify.css';
 import { AnimatePresence } from 'framer-motion';
+import Layout from '../components/ui/Layout';
 import PreLoader from '../components/ui/PreLoader';
+import PrivateRoute from './PrivateRoute';
 
 const Home = lazy(() => import('../views/home/Home'));
 const Shop = lazy(() => import('../views/shop/Shop'));
 const Cart = lazy(() => import('../views/cart/Cart'));
+const Register = lazy(() => import('../views/auth/Register'));
 const OrderDetails = lazy(() => import('../views/checkout/OrderDetails'));
 const BillingDetails = lazy(() => import('../views/checkout/BillingDetails'));
 const Payment = lazy(() => import('../views/checkout/Payment'));
-const Register = lazy(() => import('../views/auth/Register'));
-const TermsOfUse = lazy(() => import('../views/terms/TermsOfUse'));
-const NotFound = lazy(() => import('../views/404/NotFound'));
 
+// const NotFound = lazy(() => import('../views/404/NotFound'));
 
 export default function AppRoute() {
+  const location = useLocation();
 
-    const location = useLocation();
-
-    return (
-        <AnimatePresence exitBeforeEnter>
-            <Suspense fallback={<PreLoader theme="light"/>}>
-                <Switch location={location} key={location.pathname}>
-                    <PublicRoute exact path={ROUTES.HOME} component={Home}/>
-                    <PublicRoute  path={ROUTES.SHOP} component={Shop}/>
-                    <PublicRoute  path={ROUTES.CART} component={Cart}/>
-                    <PublicRoute  path={ROUTES.REGISTER} component={Register}/>
-                    <PublicRoute  path={ROUTES.TERMS_OF_USE} component={TermsOfUse}/>
-                    <PrivateRoute path={ROUTES.CHECKOUT_STEP_1} component={OrderDetails}/>
-                    <PrivateRoute path={ROUTES.CHECKOUT_STEP_2} component={BillingDetails}/>
-                    <PrivateRoute path={ROUTES.CHECKOUT_STEP_3} component={Payment}/>
-                    <PublicRoute component={NotFound}/>
-                </Switch>
-            </Suspense>
-        </AnimatePresence>
-    )
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}>
+        <Route path={ROUTES.HOME} element={<Layout />}>
+          <Route
+            index
+            element={
+              <Suspense fallback={<PreLoader />}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path={ROUTES.SHOP}
+            element={
+              <Suspense fallback={<PreLoader />}>
+                <Shop />
+              </Suspense>
+            }
+          />
+          <Route
+            path={ROUTES.CART}
+            element={
+              <Suspense fallback={<PreLoader />}>
+                <Cart />
+              </Suspense>
+            }
+          />
+          <Route
+            path={ROUTES.REGISTER}
+            element={
+              <Suspense fallback={<PreLoader />}>
+                <Register />
+              </Suspense>
+            }
+          />
+          <Route
+            path={ROUTES.CHECKOUT_STEP_1}
+            element={
+              <Suspense fallback={<PreLoader />}>
+                <PrivateRoute>
+                  <OrderDetails />
+                </PrivateRoute>
+              </Suspense>
+            }
+          />
+          <Route
+            path={ROUTES.CHECKOUT_STEP_2}
+            element={
+              <Suspense fallback={<PreLoader />}>
+                <PrivateRoute>
+                  <BillingDetails />
+                </PrivateRoute>
+              </Suspense>
+            }
+          />
+          <Route
+            path={ROUTES.CHECKOUT_STEP_3}
+            element={
+              <Suspense fallback={<PreLoader />}>
+                <PrivateRoute>
+                  <Payment />
+                </PrivateRoute>
+              </Suspense>
+            }
+          />
+        </Route>
+      </Routes>
+    </AnimatePresence>
+  );
 }
