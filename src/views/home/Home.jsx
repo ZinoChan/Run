835ea8630552258090bf7b49bbc5ff8d32dd5cce
Zineb ELGUIL <devzinochan@gmail.com>
2022-03-22 22:35:42 +0000

    redesign

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -4,11 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import nike from '../../assets/nike.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faFacebookF,
-  faTwitter,
-  faInstagram,
-} from '@fortawesome/free-brands-svg-icons';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
   const pageTransition = {
@@ -28,42 +24,12 @@ export default function Home() {
         variants={pageTransition}
         className="home position-relative d-flex align-items-center"
       >
-        <div className="container-fluid position-relative">
+        <div className="circle circle-1"></div>
+        <div className="circle circle-2"></div>
+        <div className="glass position-absolute"></div>
+        <div className="container-fluid position-relative min-vh-100">
           <div className="vector">
             <h1>Nike</h1>
-          </div>
-
-          <div className="home-text">
-            <motion.h1
-              initial={{ x: -100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{
-                type: 'spring',
-                stiffness: 260,
-                damping: 20,
-                duration: 1,
-                delay: 0.9,
-              }}
-            >
-              {' '}
-              New Collection
-            </motion.h1>
-            <motion.p
-              initial={{ x: 100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{
-                type: 'spring',
-                stiffness: 260,
-                damping: 20,
-                duration: 1,
-                delay: 0.9,
-              }}
-            >
-              {' '}
-              New level Designs,
-              <br /> explore our 2020 collection <br /> with more comfort and
-              style.
-            </motion.p>
           </div>
 
           <div className="shoe">
@@ -82,16 +48,23 @@ export default function Home() {
             </motion.div>
 
             <div className="text-center">
-              <NavLink to={SHOP} className="btn home-btn">
-                Shop Now
+              <p className="mb-3 text-body mt-3 fs-6">
+                Explore the new collections designed with <br /> comfort and
+                style in mind
+              </p>
+              <NavLink to={SHOP} className="btn home-btn fs-6 ">
+                <span> Shop Now</span>
+                <FontAwesomeIcon icon={faArrowRightLong} className="ms-2" />
               </NavLink>
             </div>
           </div>
 
-          <div className="socials">
-            <FontAwesomeIcon icon={faFacebookF} />
-            <FontAwesomeIcon icon={faTwitter} />
-            <FontAwesomeIcon icon={faInstagram} />
+          <div className="position-absolute w-100 mx-auto px-4 pb-3 bottom-0 end-0 d-flex justify-content-between">
+            <div className="socials d-flex">
+              <p className=" text-body text-uppercase fs-6">Dribbble</p>
+              <p className=" mx-4 text-body text-uppercase fs-6">Github</p>
+              <p className=" text-body text-uppercase fs-6">LinkedIn</p>
+            </div>
           </div>
         </div>
       </motion.section>
