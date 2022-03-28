6b99d7abb5800dc04ae562d13fbe4e2a7574e29e
Zineb ELGUIL <devzinochan@gmail.com>
2022-03-28 18:49:25 +0100

    remove react spinner due to issues

--- src/components/ui/PreLoader.jsx
+++ src/components/ui/PreLoader.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import {Ripple} from 'react-spinners-css';
 
-
-export default function PreLoader({theme}) {
-    return (
-        <div 
-            className={`
+export default function PreLoader({ theme }) {
+  return (
+    <div
+      className={`
                     preloader 
                     d-flex 
                     justify-content-center 
                     align-items-center 
                     vh-100 vw-100 ${theme}
                     `}
-                >
-            <Ripple color={theme === "red" ? "#fff" : "crimson"} />
-        </div>
-    )
+    >
+      Loading...
+    </div>
+  );
 }
