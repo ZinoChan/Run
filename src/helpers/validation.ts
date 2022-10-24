186cb7d1157167dfe20062f99b94a03c949889b6
ZinoChan <devzinochan@gmail.com>
2022-10-24 22:57:22 +0100

    style

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -1,37 +1,30 @@
 import * as Yup from 'yup';
 
 export const billingValidation = Yup.object({
-    fullname: Yup.string()
-        .required('Required'),
-    email: Yup.string()
-        .email('Invalid Email Address')
-        .required('Required'),
-    address: Yup.string()
-        .required('Required'),
-    country: Yup.string()
-        .required('Required'),
-    city: Yup.string()
-        .required('Required'),
-    phone: Yup.string()
-        .required('Required')
-
+  fullname: Yup.string().required('Required'),
+  email: Yup.string().email('Invalid Email Address').required('Required'),
+  address: Yup.string().required('Required'),
+  country: Yup.string().required('Required'),
+  city: Yup.string().required('Required'),
+  phone: Yup.string().required('Required'),
 });
 
 export const paymentValidation = Yup.object({
     cardName: Yup.string()
     .min(3, 'Must be at least 3 characters')
-    .required("Required"),
+    .required('Required'),
     cardNumber: Yup.string()
       .min(13, "Credit Card Number can't be less then 13")
-      .required("Required"),
+    .required('Required'),
     cvvCode: Yup.string()
       .min(3, 'Cvv Code must be 3 for Mastercard or Visa and 4 for Express')
       .max(4, 'Cvv Code must be 3 for Mastercard or Visa and 4 for Express')
-      .required("Required"),
+    .required('Required'),
     date: Yup.date()
         .min(new Date(), `Date must be later then ${new Date()}`)
         .required('Required'),
     terms: Yup.boolean()
         .required('Required')
-        .oneOf([true], 'You must accept the terms of use')
+    .oneOf([true], 'You must accept the terms of use'),
+});
 });
