596371af236463c057c5276bd8ed56d42be133cc
ZinoChan <devzinochan@gmail.com>
2023-05-01 20:03:28 +0100

    Refactor ProductsList component to use TypeScript interface for props and arrow function syntax for better readability and maintainability

--- src/components/shop/ProductsList.tsx
+++ src/components/shop/ProductsList.tsx
@@ -1,20 +1,21 @@
 import { IProductRes } from '@/types/products.interface';
 import ProductItem from './ProductItem';
 
-export default function ProductsList({
-  products,
-}: {
+interface ProductsListProps {
   products: IProductRes[];
-}) {
-  return (
-    <div className="product-list" style={{ marginBottom: '6rem' }}>
-      <div className="container-fluid items-stretch">
-        <div className="row">
-          {products.map((product) => (
-            <ProductItem product={product} key={product.id} />
-          ))}
-        </div>
+}
+
+const ProductsList: React.FC<ProductsListProps> = ({ products }) => (
+  <div className="product-list" style={{ marginBottom: '6rem' }}>
+    <div className="container-fluid items-stretch">
+      <div className="row">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
       </div>
     </div>
-  );
-}
+  </div>
+);
+
+export default ProductsList;
+
