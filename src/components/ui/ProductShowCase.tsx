fbb5c6e0f2041cf4d4d5e1f8a3bb72c8ba25956c
ZinoChan <devzinochan@gmail.com>
2023-05-07 19:32:32 +0100

    Add image loading functionality to ProductShowCase component with custom render functions

--- src/components/ui/ProductShowCase.tsx
+++ src/components/ui/ProductShowCase.tsx
@@ -1,12 +1,27 @@
 import { ColorImg } from '@/types/products.interface';
-import ImageGallery from 'react-image-gallery';
+import ImageGallery, { ReactImageGalleryItem } from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import ImgLoader from './ImgLoader';
 
 const ProductShowCase = ({ imgs }: { imgs: ColorImg[] }) => {
   const showCaseImages = imgs?.map(({ img }) => ({
     original: img,
     thumbnail: img,
   }));
+  const renderItem = (item: ReactImageGalleryItem) => (
+    <ImgLoader
+      src={item.original}
+      alt={item.originalAlt ? item.originalAlt : "gallery-img"}
+      className='image-gallery-image'
+    />
+  )
+  const renderThumbInner = (item: ReactImageGalleryItem) => (
+    <ImgLoader
+      src={item.thumbnail ? item.thumbnail : ''}
+      alt={item.thumbnailAlt ? item.thumbnailAlt : "thumbnail_img"}
+      className="image-gallery-thumbnail-image"
+    />
+  );
   return (
     <ImageGallery
       showPlayButton={false}
@@ -14,6 +29,8 @@ const ProductShowCase = ({ imgs }: { imgs: ColorImg[] }) => {
       thumbnailPosition="left"
       lazyLoad
       showNav={false}
+      renderItem={renderItem}
+      renderThumbInner={renderThumbInner}
     />
   );
 };
