6f29871339c5cab88afcef71fad9081cdd5582d9
ZinoChan <devzinochan@gmail.com>
2023-05-07 19:34:10 +0100

    Refactor BillingDetails and Payment to use react-hook-form for form handling and validation

--- src/views/checkout/BillingDetails.tsx
+++ src/views/checkout/BillingDetails.tsx
@@ -1,10 +1,11 @@
 import { CHECKOUT_STEP_1, CHECKOUT_STEP_3 } from '../../constants/routes';
 import { NavLink, useNavigate } from 'react-router-dom';
 import TextInput, { Label } from '../../components/checkout/TextInput';
-import { Form, Formik, FormikProps } from 'formik';
 import { billingValidation } from '../../helpers/validation';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-type Values = {
+export type BillingValues = {
   fullname: string;
   email: string;
   address: string;
@@ -14,87 +15,93 @@ type Values = {
 };
 
 const BillingDetails = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<BillingValues>({ resolver: yupResolver(billingValidation) });
   const navigate = useNavigate();
+  const onSubmit = (BillingValues: BillingValues) => {
+    navigate(CHECKOUT_STEP_3, { replace: true });
+  };
 
   return (
-    <section className=" d-flex align-items-center mx-auto min-vh-100">
+    <section className="d-flex align-items-center mx-auto min-vh-100">
       <div className="glass"></div>
       <div className="container-fluid">
         <h1 className="fs-1 mb-5 text-center">Billing Details</h1>
-        <Formik
-          initialValues={{
-            fullname: '',
-            email: '',
-            address: '',
-            country: '',
-            city: '',
-            phone: '',
-          }}
-          validationSchema={billingValidation}
-          onSubmit={(values, { resetForm }) => {
-            resetForm();
-            navigate(CHECKOUT_STEP_3, { replace: true });
-          }}
-        >
-          {(props: FormikProps<Values>) => (
-            <Form className="w-75 mx-auto">
-              <div className="row">
-                <div className="col-xl-6 form-group mb-3">
-                  <Label label="Full Name*" />
-                  <TextInput
-                    name="fullname"
-                    type="text"
-                    className="form-control"
-                  />
-                </div>
-                <div className="col-xl-6 form-group mb-3">
-                  <Label label="Email*" />
-                  <TextInput
-                    name="email"
-                    type="email"
-                    className="form-control"
-                  />
-                </div>
-                <div className="col-xl-6 form-group mb-3">
-                  <Label label="House Number And Street Name*" />
-                  <TextInput
-                    name="address"
-                    type="text"
-                    className="form-control"
-                  />
-                </div>
-                <div className="col-xl-6 form-group mb-3">
-                  <Label label="Country*" />
-                  <TextInput
-                    name="country"
-                    type="text"
-                    className="form-control"
-                  />
-                </div>
-                <div className="col-xl-6 form-group mb-3">
-                  <Label label="Town / City*" />
-                  <TextInput name="city" type="text" className="form-control" />
-                </div>
-                <div className="col-xl-6 form-group mb-3">
-                  <Label label="Phone*" />
-                  <TextInput
-                    name="phone"
-                    type="phone"
-                    className="form-control"
-                  />
-                </div>
-              </div>
-              <div className="d-flex justify-content-between mt-2">
-                <NavLink to={CHECKOUT_STEP_1} className="btn btn-dark">
-                  Back
-                </NavLink>
-                <button className="btn btn-dark" type="submit">
-                  Continue
-                </button>
-              </div>
-            </Form>
-          )}
-        </Formik>
+        <form className="w-75 mx-auto" onSubmit={handleSubmit(onSubmit)}>
+          <div className="row">
+            <div className="col-xl-6 form-group mb-3">
+              <Label label="Full Name*" />
+              <TextInput
+                name="fullname"
+                type="text"
+                className="form-control"
+                register={register}
+                error={errors.fullname}
+              />
+            </div>
+            <div className="col-xl-6 form-group mb-3">
+              <Label label="Email*" />
+              <TextInput
+                name="email"
+                type="email"
+                className="form-control"
+                register={register}
+                error={errors.email}
+              />
+            </div>
+            <div className="col-xl-6 form-group mb-3">
+              <Label label="House Number And Street Name*" />
+              <TextInput
+                name="address"
+                type="text"
+                className="form-control"
+                register={register}
+                error={errors.address}
+              />
+            </div>
+            <div className="col-xl-6 form-group mb-3">
+              <Label label="Country*" />
+              <TextInput
+                name="country"
+                type="text"
+                className="form-control"
+                register={register}
+                error={errors.country}
+              />
+            </div>
+            <div className="col-xl-6 form-group mb-3">
+              <Label label="Town / City*" />
+              <TextInput
+                name="city"
+                type="text"
+                className="form-control"
+                register={register}
+                error={errors.city}
+              />
+            </div>
+            <div className="col-xl-6 form-group mb-3">
+              <Label label="Phone*" />
+              <TextInput
+                name="phone"
+                type="phone"
+                className="form-control"
+                register={register}
+                error={errors.phone}
+              />
+            </div>
+          </div>
+          <div className="d-flex justify-content-between mt-2">
+            <NavLink to={CHECKOUT_STEP_1} className="btn btn-dark">
+              Back
+            </NavLink>
+            <button className="btn btn-dark" type="submit">
+              Continue
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   );
--- src/views/checkout/Payment.tsx
+++ src/views/checkout/Payment.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { CHECKOUT_STEP_2, SHOP } from '@/constants/routes';
 import { Link, useNavigate } from 'react-router-dom';
-import { Formik } from 'formik';
 import { paymentValidation } from '@/helpers/validation';
 import TextInput, { Label } from '@/components/checkout/TextInput';
 import { toast } from 'react-hot-toast';
@@ -11,6 +10,8 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import CheckBox from '@/components/checkout/CheckBox';
 import { useAppDispatch } from '@/store';
 import { clearCart } from '@/reducers/cartReducer';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 export default function Payment() {
   const [isOpenModal, setOpenModal] = useState(false);
@@ -24,74 +25,79 @@ export default function Payment() {
 
   const dispatch = useAppDispatch();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(paymentValidation),
+  });
+
+  const onSubmit = (values: any) => {
+    console.log(values);
+    openModal();
+    toast.success('Order Successfully Confirmed');
+    dispatch(clearCart());
+  };
+
   return (
     <section className="d-flex align-items-center mx-auto min-vh-100">
       <div className="container-fluid">
         <h1 className="fs-2 fw-bold mb-3 text-center">Payment</h1>
-        <Formik
-          initialValues={{
-            cardName: '',
-            cardNumber: '',
-            cvvCode: '',
-            date: '',
-            terms: '',
-          }}
-          validationSchema={paymentValidation}
-          onSubmit={(values, { resetForm }) => {
-            openModal();
-            toast.success('Order Successfully Confirmed');
-            resetForm();
-            dispatch(clearCart());
-          }}
+        <form
+          className="w-50 mx-auto"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          {({ handleSubmit }) => (
-            <form
-              className="w-50 mx-auto"
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleSubmit();
-              }}
-            >
-              <div className="form-group mb-3">
-                <Label label="Name on Card*" />
-                <TextInput
-                  name="cardName"
-                  type="text"
-                  className="form-control"
-                />
-              </div>
-              <div className="form-group mb-3">
-                <Label label="Card Number*" />
-                <TextInput
-                  name="cardNumber"
-                  type="text"
-                  className="form-control"
-                />
-              </div>
-              <div className="form-group mb-3">
-                <Label label="Cvv Code*" />
-                <TextInput
-                  name="cvvCode"
-                  type="text"
-                  className="form-control"
-                />
-              </div>
-              <div className="form-group mb-3">
-                <Label label="Expirity Date*" />
-                <TextInput name="date" type="date" className="form-control" />
-              </div>
-              <CheckBox name="terms" />
-              <div className="d-flex justify-content-between mt-2">
-                <Link to={CHECKOUT_STEP_2} className="btn btn-dark">
-                  Back
-                </Link>
-                <button className="btn btn-dark" type="submit">
-                  Confirm
-                </button>
-              </div>
-            </form>
-          )}
-        </Formik>
+          <div className="form-group mb-3">
+            <Label label="Name on Card*" />
+            <TextInput
+              name="cardName"
+              type="text"
+              className="form-control"
+              register={register}
+              error={errors.cardName}
+            />
+          </div>
+          <div className="form-group mb-3">
+            <Label label="Card Number*" />
+            <TextInput
+              name="cardNumber"
+              type="text"
+              className="form-control"
+              register={register}
+              error={errors.cardNumber}
+            />
+          </div>
+          <div className="form-group mb-3">
+            <Label label="Cvv Code*" />
+            <TextInput
+              name="cvvCode"
+              type="text"
+              className="form-control"
+              register={register}
+              error={errors.cvvCode}
+            />
+          </div>
+          <div className="form-group mb-3">
+            <Label label="Expirity Date*" />
+            <TextInput
+              name="date"
+              type="date"
+              className="form-control"
+              register={register}
+              error={errors.date}
+            />
+          </div>
+          <CheckBox name="terms" register={register} error={errors.terms} />
+          <div className="d-flex justify-content-between mt-2">
+            <Link to={CHECKOUT_STEP_2} className="btn btn-dark">
+              Back
+            </Link>
+            <button className="btn btn-dark" type="submit">
+              Confirm
+            </button>
+          </div>
+        </form>
         <ConfirmModal isOpen={isOpenModal} closeModal={closeModal}>
           <FontAwesomeIcon
             icon={faCheckCircle}
