d15d03477cf4e40d3684c5b2b4ed760fe071700f
ZinoChan <devzinochan@gmail.com>
2023-05-07 19:32:05 +0100

    Refactor TextInput component to use react-hook-form for form registration and error handling

--- src/components/checkout/TextInput.tsx
+++ src/components/checkout/TextInput.tsx
@@ -1,18 +1,33 @@
-import { FieldHookConfig, useField } from 'formik';
+import { UseFormRegister, Path, FieldValues } from 'react-hook-form';
+
+type Props<T extends FieldValues> = {
+  name: Path<T>;
+  type: string;
+  className?: string;
+  register: UseFormRegister<T>;
+  error: any;
+};
 
 export const Label = ({ label }: { label: string }) => (
   <label className="text-capitalize">{label}</label>
 );
 
-export default function TextInput(props: FieldHookConfig<string>) {
-  const [field, meta] = useField(props);
-
+const TextInput = <T extends FieldValues>({
+  name,
+  type,
+  className,
+  register,
+  error,
+}: Props<T>) => {
   return (
     <>
-      <input className={props?.className || ''} {...field} type={props.type} />
-      {meta.touched && meta.error ? (
-        <div className="error text-danger">{meta.error}</div>
-      ) : null}
+      <input type={type} className={className || ''} {...register(name)} />
+      {error ? <div className="error text-danger">{error.message}</div> : null}
     </>
   );
-}
+};
+
+export default TextInput;
+
+
+
